58b0d66813458f8cfa09ebdc563428897b1e9b11
evilBird <travishenspeter@Traviss-MacBook-Pro-2.local>
2014-09-04 12:31:41 -0500

    Initial Commit

---
+++ BlackBox.UI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlackBox.UI
+//
+//  Created by Travis Henspeter on 9/4/14.
+//  Copyright (c) 2014 birdSound LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlackBox.UI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlackBox.UI
+//
+//  Created by Travis Henspeter on 9/4/14.
+//  Copyright (c) 2014 birdSound LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
