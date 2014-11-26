a2ac127f1aa01d9953d49851dd891c1fcc1360d8
evilBird <travishenspeter@Traviss-MacBook-Pro-2.local>
2014-11-26 13:34:00 -0600

    Added GPUImage class

---
+++ BSDGPUImage.h
@@ -0,0 +1,13 @@
+//
+//  BSDGPUImage.h
+//  BlackBox.UI
+//
+//  Created by Travis Henspeter on 11/26/14.
+//  Copyright (c) 2014 birdSound LLC. All rights reserved.
+//
+
+#import "BSDObject.h"
+
+@interface BSDGPUImage : BSDObject
+
+@end
