2c874a5124457a6a550ea6bddbae60dee330bac3
evilBird <travishenspeter@Traviss-MacBook-Pro.local>
2015-02-22 02:32:16 -0600

    trim whitespace from message strings

---
+++ Bb/BbDictionary.m
@@ -0,0 +1,13 @@
+//
+//  BbDictionary.m
+//  Visual Cocoa
+//
+//  Created by Travis Henspeter on 2/22/15.
+//  Copyright (c) 2015 birdSound. All rights reserved.
+//
+
+#import "BbDictionary.h"
+
+@implementation BbDictionary
+
+@end
---
+++ Bb/BbGET.m
@@ -0,0 +1,13 @@
+//
+//  BbGET.m
+//  Visual Cocoa
+//
+//  Created by Travis Henspeter on 2/21/15.
+//  Copyright (c) 2015 birdSound. All rights reserved.
+//
+
+#import "BbGET.h"
+
+@implementation BbGET
+
+@end
