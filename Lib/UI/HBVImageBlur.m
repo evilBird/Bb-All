089f2e796640d9794c54534ccd60200595295102
evilBird <travishenspeter@Traviss-MacBook-Pro.local>
2015-05-13 19:03:23 -0500

    added image blur again

--- Lib/UI/HBVImageBlur.m
+++ Lib/UI/HBVImageBlur.m
@@ -15,8 +15,8 @@ CGFloat clip_blur(CGFloat blur){
     if (blur > 1.0f) {
         blur = 1.0f;
     }
-    if (blur < 0.025f) {
-        blur = 0.025f;
+    if (blur < 0.0f) {
+        blur = 0.0f;
     }
     
     return blur;
@@ -53,43 +53,39 @@ CGFloat clip_blur(CGFloat blur){
 
 + (UIImage *)vImageApplyBlurOnImage:(UIImage *)imageToBlur withRadius:(CGFloat)blurRadius
 {
-    if (imageToBlur != nil) {
-        blurRadius = clip_blur(blurRadius);
-        
-        int boxSize = (int)(blurRadius * 100); boxSize -= (boxSize % 2) + 1;
-        CGImageRef rawImage = imageToBlur.CGImage; vImage_Buffer inBuffer, outBuffer;
-        vImage_Error error;
-        void *pixelBuffer;
-        CGDataProviderRef inProvider = CGImageGetDataProvider(rawImage);
-        CFDataRef inBitmapData = CGDataProviderCopyData(inProvider);
-        inBuffer.width = CGImageGetWidth(rawImage);
-        inBuffer.height = CGImageGetHeight(rawImage);
-        inBuffer.rowBytes = CGImageGetBytesPerRow(rawImage);
-        inBuffer.data = (void*)CFDataGetBytePtr(inBitmapData);
-        pixelBuffer = malloc(CGImageGetBytesPerRow(rawImage) * CGImageGetHeight(rawImage));
-        outBuffer.data = pixelBuffer;
-        outBuffer.width = CGImageGetWidth(rawImage);
-        outBuffer.height = CGImageGetHeight(rawImage);
-        outBuffer.rowBytes = CGImageGetBytesPerRow(rawImage);
-        error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
-        
-        if (error) {
-            NSLog(@"error from convolution %ld", error);
-        }
-        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-        CGContextRef ctx = CGBitmapContextCreate(outBuffer.data, outBuffer.width, outBuffer.height, 8, outBuffer.rowBytes, colorSpace, CGImageGetBitmapInfo(imageToBlur.CGImage));
-        CGImageRef imageRef = CGBitmapContextCreateImage (ctx);
-        UIImage *returnImage = [UIImage imageWithCGImage:imageRef];
-        //clean up
-        CGContextRelease(ctx);
-        CGColorSpaceRelease(colorSpace);
-        free(pixelBuffer);
-        CFRelease(inBitmapData);
-        CGImageRelease(imageRef);
-        return returnImage;
-
+    blurRadius = clip_blur(blurRadius);
+    
+    int boxSize = (int)(blurRadius * 100); boxSize -= (boxSize % 2) + 1;
+    CGImageRef rawImage = imageToBlur.CGImage; vImage_Buffer inBuffer, outBuffer;
+    vImage_Error error;
+    void *pixelBuffer;
+    CGDataProviderRef inProvider = CGImageGetDataProvider(rawImage);
+    CFDataRef inBitmapData = CGDataProviderCopyData(inProvider);
+    inBuffer.width = CGImageGetWidth(rawImage);
+    inBuffer.height = CGImageGetHeight(rawImage);
+    inBuffer.rowBytes = CGImageGetBytesPerRow(rawImage);
+    inBuffer.data = (void*)CFDataGetBytePtr(inBitmapData);
+    pixelBuffer = malloc(CGImageGetBytesPerRow(rawImage) * CGImageGetHeight(rawImage));
+    outBuffer.data = pixelBuffer;
+    outBuffer.width = CGImageGetWidth(rawImage);
+    outBuffer.height = CGImageGetHeight(rawImage);
+    outBuffer.rowBytes = CGImageGetBytesPerRow(rawImage);
+    error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
+    
+    if (error) {
+        NSLog(@"error from convolution %ld", error);
     }
-    return [UIImage imageNamed:@"placeholder_profile"];
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef ctx = CGBitmapContextCreate(outBuffer.data, outBuffer.width, outBuffer.height, 8, outBuffer.rowBytes, colorSpace, CGImageGetBitmapInfo(imageToBlur.CGImage));
+    CGImageRef imageRef = CGBitmapContextCreateImage (ctx);
+    UIImage *returnImage = [UIImage imageWithCGImage:imageRef];
+    //clean up
+    CGContextRelease(ctx);
+    CGColorSpaceRelease(colorSpace);
+    free(pixelBuffer);
+    CFRelease(inBitmapData);
+    CGImageRelease(imageRef);
+    return returnImage;
 }
 
 
