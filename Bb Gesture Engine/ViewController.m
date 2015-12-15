526fddd741ed7ce13d5a7ca5255fddd348870257
evilBird <travishenspeter@traviss-macbook-pro.local>
2015-12-15 12:19:14 -0600

    Initial Commit

---
+++ Bb Gesture Engine/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Bb Gesture Engine
+//
+//  Created by Travis Henspeter on 12/15/15.
+//  Copyright © 2015 birdSound. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Bb Gesture EngineTests/Bb_Gesture_EngineTests.m
@@ -0,0 +1,39 @@
+//
+//  Bb_Gesture_EngineTests.m
+//  Bb Gesture EngineTests
+//
+//  Created by Travis Henspeter on 12/15/15.
+//  Copyright © 2015 birdSound. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Bb_Gesture_EngineTests : XCTestCase
+
+@end
+
+@implementation Bb_Gesture_EngineTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
